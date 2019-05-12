30cc5513fde901311eb935a39cffc22b87f4496d
kenneth.marete <maretekent@gmail.com>
2019-05-12 13:34:47 +0300

    Iterating through the lines, search for matching words in line

--- src/lib.rs
+++ src/lib.rs
@@ -33,7 +33,13 @@ pub fn run(config: Config) -> Result<(), Box<Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str ) -> Vec<&'a str>{
-    vec![]
+    let mut results = Vec::new();
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line)
+        }
+    }
+    results
 }
 
 #[cfg(test)]
@@ -45,8 +51,8 @@ mod test{
         let query = "duct";
         let contents = "\
         Rust:
-        safe, fast, productive.
-        Pick three.";
+safe, fast, productive.
+Pick three.";
 
         assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
